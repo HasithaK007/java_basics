b39f5d48dc92fdfe10358e3089d1727922f6fc37
HasithaKarawita <hasithasaberion@gmail.com>
2025-11-16 10:18:32 +0530

    Java First Program

---
+++ src/com/w3school/NestedStat.java
@@ -0,0 +1,15 @@
+package com.w3school;
+
+public class NestedStat {
+    public static void main(String[] args) {
+        int x=15;
+        int y =25;
+        if(x>10){
+            System.out.println("x is greater than value");
+
+            if(y>15){
+                System.out.println("y also greater than value");
+            }
+        }
+    }
+}
