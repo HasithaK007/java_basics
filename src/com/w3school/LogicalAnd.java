bccb404d392c0e7e89e15c1088eb673e14700975
HasithaKarawita <hasithasaberion@gmail.com>
2025-11-16 10:48:47 +0530

    Java First Program

---
+++ src/com/w3school/LogicalAnd.java
@@ -0,0 +1,12 @@
+package com.w3school;
+
+public class LogicalAnd {
+    public static void main(String[] args) {
+        int a=200;
+        int b=30;
+        int c=500;
+        if(a>b && c>a){
+            System.out.println("both conditions true");
+        }
+    }
+}
